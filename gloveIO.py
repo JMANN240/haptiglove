0d6be941c074f985791e2e93cc209715c5476ea9
Jarrett Woo <61482222+JarrettWoo@users.noreply.github.com>
2022-12-07 08:08:12 -0800

    Add files via upload

--- gloveIO.py
+++ gloveIO.py
@@ -1,5 +1,6 @@
 from dataglove import *
 import serial
+import msvcrt
 from time import sleep
 
 THUMB = 0
@@ -9,7 +10,7 @@ RING = 3
 PINKY = 4
 PALM = 5
 
-ser = serial.Serial('COM4', 115200)
+ser = serial.Serial('COM12', 115200)
 ser.flush()
 
 def get_pressure_data():
@@ -46,19 +47,19 @@ def get_pressure_data():
 def update_haptics(datagloveIO, normalizedPressureData):
 	# Sends haptics to glove
 	# Forte_SendHaptic(leftHand, 4, 0, 1.0)
-	pass
-	# if (len(normalizedPressureData) == 5):
-
-	# 	Forte_SendHaptic(datagloveIO, THUMB, 0, normalizedPressureData[0])
-	# 	Forte_SendHaptic(datagloveIO, INDEX, 0, normalizedPressureData[1])
-	# 	Forte_SendHaptic(datagloveIO, MIDDLE, 0, normalizedPressureData[2])
-	# 	Forte_SendHaptic(datagloveIO, RING, 0, normalizedPressureData[3])
-	# 	Forte_SendHaptic(datagloveIO, PINKY, 0, normalizedPressureData[4])
-
+	# pass
+	try:
+		Forte_SendHaptic(datagloveIO, THUMB, 0, normalizedPressureData)
+		Forte_SendHaptic(datagloveIO, INDEX, 0, normalizedPressureData)
+		Forte_SendHaptic(datagloveIO, MIDDLE, 0, normalizedPressureData)
+		Forte_SendHaptic(datagloveIO, RING, 0, normalizedPressureData)
+		Forte_SendHaptic(datagloveIO, PINKY, 0, normalizedPressureData)
+	except(GloveDisconnectedException):
+		print("Glove is Disconnected")
+		
 def send_sensors():
 	# [0, -16, 16, 23, 104, 115, 127, 86, 63, 116]
 	try:
-		# leftHand = Forte_CreateDataGloveIO(1, "")
 		# sleep(0.1)
 
 		sensors = Forte_GetFullReport(leftHand)
@@ -69,7 +70,6 @@ def send_sensors():
 		
 		print(sensors)
 
-		# Forte_DestroyDataGloveIO(leftHand)
 		sleep(0.05)
 		ser.write(f"{str(thumb).rjust(3, '0')},{str(index).rjust(3, '0')},{str(middle).rjust(3, '0')},{str(middle).rjust(3, '0')},{str(middle).rjust(3, '0')}".encode("utf-8"))
 	except(KeyboardInterrupt):
@@ -84,12 +84,28 @@ i = 0
 
 if __name__ == '__main__':
 
+	mode = input("Select mode (1 | 2 | 3):\n1. Haptic + Visual\n2. Haptic Only\n3. Visual Only ")
+	mode = int(mode)
+	file = ['Haptic+Visual', 'HapticOnly', 'VisualOnly']
+	
 	leftHand = Forte_CreateDataGloveIO(1, "")
+
+
 
 	while True:
 		try:
 			send_sensors()
-			update_haptics(leftHand, get_pressure_data())
+			pressure_data = get_pressure_data()
+			pressure_data = pressure_data[0]
+			
+			if msvcrt.kbhit():
+				print(msvcrt.getch())
+				if msvcrt.getch() == b' ':
+					outputFile = open(f"Data_{file[mode -1]}.txt", "a")
+					outputFile.write(f"{pressure_data}\n")
+					outputFile.close()
+			if mode != 3:
+				update_haptics(leftHand, pressure_data)
 		except(KeyboardInterrupt):
 			Forte_SilenceHaptics(leftHand)
 			Forte_DestroyDataGloveIO(leftHand)
